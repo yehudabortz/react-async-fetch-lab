924baf154a0f787718a3bd4a2e1c2c2acd4be9b5
Yehuda Bortz <yehudabortz@icloud.com>
2021-04-16 16:53:03 -0500

    commit tree 9371720c2591074d494187cb83ea7e2a7ae75fb1

--- src/App.js
+++ src/App.js
@@ -1,1 +1,17 @@
 // create your App component here
+
+import React from "react";
+
+class App extends React.Component {
+  componentDidMount() {
+    console.log("hi");
+    fetch("http://api.open-notify.org/astros.json")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }
+  render() {
+    return <div>hi</div>;
+  }
+}
+
+export default App;
